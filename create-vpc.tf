ab3c71c06ab4b8b0ea090f8f703ab95c42d1d72f
safeeha <eemarabs@gmail.com>
2024-11-30 06:51:32 +0100

    new comment

--- create-vpc.tf
+++ create-vpc.tf
@@ -1,4 +1,4 @@
-# Create a new VPC to launch instances
+# Create a new VPC to launch instances for EC2 instances
 resource "aws_vpc" "custom" {
   cidr_block           = 10.0.0.0/16
   enable_dns_support   = true
