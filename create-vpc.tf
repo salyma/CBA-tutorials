ab091657e91f97d498285d072f29180a03c8cac9
safeeha <eemarabs@gmail.com>
2024-11-30 06:38:00 +0100

    new commit

--- create-vpc.tf
+++ create-vpc.tf
@@ -1,4 +1,4 @@
-# Create a new VPC to launch our EC2 instances
+# Create a new VPC to launch instances
 resource "aws_vpc" "custom" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
