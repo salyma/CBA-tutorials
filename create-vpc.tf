b9368cd9d1d4a503e8638da37d1c52df0d68d1bd
safeeha <eemarabs@gmail.com>
2024-11-30 06:44:02 +0100

    updated cidr

--- create-vpc.tf
+++ create-vpc.tf
@@ -1,6 +1,6 @@
 # Create a new VPC to launch instances
 resource "aws_vpc" "custom" {
-  cidr_block           = var.vpc_cidr
+  cidr_block           = 10.0.0.0/16
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
